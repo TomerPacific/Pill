51e94cdab3281d38f07994ebb024bd40e606f599
unknown <tomerpacific@gmail.com>
2021-10-13 21:38:55 +0300

    Pill class to hold pill data

--- pill/lib/model/PillToTake.dart
+++ pill/lib/model/PillToTake.dart
@@ -1,6 +1,9 @@
 
-class PillToTakeModel {
+import 'package:pill/model/PillRegiment.dart';
+
+class PillToTake {
   String pillName;
   double pillWeight;
-  
+  PillRegiment pillRegiment;
+  String description;
 }
