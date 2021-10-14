c13e6ff4b21e3254b8575337a54c14d5a936a552
unknown <tomerpacific@gmail.com>
2021-10-14 18:26:45 +0300

    Pill allowing PillToTake to be encoded/decoded

--- pill/lib/model/PillToTake.dart
+++ pill/lib/model/PillToTake.dart
@@ -1,4 +1,4 @@
-
+import 'dart:convert';
 import 'package:pill/model/PillRegiment.dart';
 
 class PillToTake {
@@ -6,4 +6,38 @@ class PillToTake {
   double pillWeight;
   PillRegiment pillRegiment;
   String description;
+
+  PillToTake({
+    this.pillName,
+    this.pillWeight,
+    this.pillRegiment,
+    this.description
+  });
+
+  factory PillToTake.fromJson(Map<String, dynamic> jsonData) {
+    return PillToTake(
+      pillName: jsonData['pillName'],
+      pillWeight: jsonData['pillWeight'],
+      pillRegiment: jsonData['pillRegiment'],
+      description: jsonData['description']
+    );
+  }
+
+  static Map<String, dynamic> toMap(PillToTake pill) => {
+    'pillName': pill.pillName,
+    'pillWeight': pill.pillWeight,
+    'pillRegiment': pill.pillRegiment,
+    'description': pill.description
+  };
+
+  static String encode(List<PillToTake> pills) => json.encode(
+    pills
+        .map<Map<String, dynamic>>((pill) => PillToTake.toMap(pill))
+        .toList(),
+  );
+
+  static List<PillToTake> decode(String pills) =>
+      (json.decode(pills) as List<dynamic>)
+          .map<PillToTake>((pill) => PillToTake.fromJson(pill))
+          .toList();
 }
