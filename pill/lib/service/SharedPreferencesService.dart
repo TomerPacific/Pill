4cb8537e8f99c48ea0dca6e79d0ecd3f504e8ef5
unknown <tomerpacific@gmail.com>
2021-10-14 18:28:15 +0300

    Pill adding logic to set/get pills to take per date

--- pill/lib/service/SharedPreferencesService.dart
+++ pill/lib/service/SharedPreferencesService.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:pill/model/PillToTake.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesService {
@@ -13,6 +14,16 @@ class SharedPreferencesService {
     if (_sharedPreferences == null) {
       _sharedPreferences = await SharedPreferences.getInstance();
     }
+  }
+
+  void setPillsForDate(String currentDate, List<PillToTake> pills) {
+    _sharedPreferences.setString(currentDate, PillToTake.encode(pills));
+  }
+
+  List<PillToTake> getPillsToTakeForDate(String currentDate) {
+      String encodedPills = _sharedPreferences.getString(currentDate);
+      final List<PillToTake> pills = PillToTake.decode(encodedPills);
+      return pills;
   }
 
 }
