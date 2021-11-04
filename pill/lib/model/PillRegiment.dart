8511c32f27f5277ab6577cfec9f2f96872f6bf32
unknown <tomerpacific@gmail.com>
2021-11-04 20:19:49 +0200

    Pill creating class to allow converting saved pillRegiment object to enum

--- pill/lib/model/PillRegiment.dart
+++ pill/lib/model/PillRegiment.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/foundation.dart';
+
 enum PillRegiment {
   DAILY,
   WEEKLY,
   MONTHLY
+}
+
+class PillRegimentHelper {
+  static PillRegiment getPillRegiment(dynamic pillRegimentObject) {
+    return PillRegiment.values.firstWhere((element) => describeEnum(element) == pillRegimentObject);
+  }
 }
