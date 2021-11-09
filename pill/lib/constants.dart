08d88827cd897c1304532fe155664355296f508f
unknown <tomerpacific@gmail.com>
2021-11-09 22:50:44 +0200

    Pill creating constants class

---
+++ pill/lib/constants.dart
@@ -0,0 +1,5 @@
+
+const String PILL_NAME_KEY = "pillName";
+const String PILL_WEIGHT_KEY = "pillWeight";
+const String PILL_REGIMENT_KEY = "pillRegiment";
+const String PILL_DESCRIPTION_KEY = "description";
