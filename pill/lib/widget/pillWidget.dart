9d7c40f5e6f15185deb40d7d1dd5c8d53d4f3c14
unknown <tomerpacific@gmail.com>
2021-11-24 22:22:45 +0200

    Pill adding constructor and UI for pillWidget

--- pill/lib/widget/pillWidget.dart
+++ pill/lib/widget/pillWidget.dart
@@ -1,22 +1,32 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pill/model/PillToTake.dart';
 
 class PillWidget extends StatelessWidget {
 
-  Future<void> _init() async {
+  const PillWidget({
+    Key key,
+    @required this.pillToTake
+  }) : super(key: key);
 
-  }
+  final PillToTake pillToTake;
 
   @override
   Widget build(BuildContext context) {
     return new Container(
-      child: FutureBuilder<void>(
-       builder: (context, snapshot) {
-         return new Text(
-             "Pill Data"
-          );
-        },
-        future: _init(),
+      color: Colors.greenAccent,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          new Text(
+            this.pillToTake.pillName,
+            style:  new TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+            ),
+          )
+        ],
       )
     );
   }
