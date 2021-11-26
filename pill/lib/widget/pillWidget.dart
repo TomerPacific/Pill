2a23189084cf8a5a5a50933359a3250d6f4de341
unknown <tomerpacific@gmail.com>
2021-11-26 22:13:25 +0200

    Pill changing pillWidget to be clickable

--- pill/lib/widget/pillWidget.dart
+++ pill/lib/widget/pillWidget.dart
@@ -17,47 +17,51 @@ class PillWidget extends StatelessWidget {
     return new Container(
       color: Colors.greenAccent,
       child: new Card(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              new Text(
-                this.pillToTake.pillName,
-                style:  new TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold
-                ),
-              )
-            ]
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                    this.pillToTake.pillImage,
-                  width: 100,
-                  height: 100
+        child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {},
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      new Text(
+                        this.pillToTake.pillName,
+                        style:  new TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ]
+                    ),
+                    new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                            this.pillToTake.pillImage,
+                          width: 100,
+                          height: 100
+                        )
+                      ],
+                    ),
+                    new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      new Text(
+                          this.pillToTake.pillRegiment,
+                          style:  new TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold
+                          )
+                      )
+                      ],
+                    )
+                  ]
                 )
-              ],
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              new Text(
-                  this.pillToTake.pillRegiment,
-                  style:  new TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold
-                  )
-              )
-              ],
-            )
-          ]
-        )
-      ),
-      );
+           ),
+      )
+    );
   }
 
 }
