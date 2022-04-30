35fb114b304c5c5ccda71a0db493d0495857a94f
unknown <tomerpacific@gmail.com>
2022-04-30 15:40:33 +0300

    Pill adding adding bloc builder inside dayWidget

--- pill/lib/widget/dayWidget.dart
+++ pill/lib/widget/dayWidget.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pill/bloc/PillState.dart';
+import 'package:pill/bloc/pill_bloc.dart';
 import 'package:pill/model/PillToTake.dart';
 import 'package:pill/service/DateService.dart';
 import 'package:pill/service/SharedPreferencesService.dart';
@@ -46,44 +49,54 @@ class DayWidgetState extends State<DayWidget> {
     });
   }
 
-  Widget drawPills() {
-    return _pillsToTake.length == 0 ?
-    new Padding(
-      padding: const EdgeInsets.only(top: 20),
-        child: new Text(
-            "You do not have to take any pills today 😀",
-            style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-        )
+  Widget drawPills(PillState state) {
+    if (state is PillLoading) {
+      return const CircularProgressIndicator();
+    }
+    if (state is PillLoaded) {
+      return state.pillsToTake.length == 0 ?
+      new Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: new Text(
+              "You do not have to take any pills today 😀",
+              style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+          )
       )
-     :
-    Expanded(
-      child: SizedBox(
-        height: 200.0,
-        child:  ListView.builder(
-            itemCount: _pillsToTake.length,
-            itemBuilder:
-                (_, index) =>
+          :
+      Expanded(
+          child: SizedBox(
+            height: 200.0,
+            child:  ListView.builder(
+                itemCount: state.pillsToTake.length,
+                itemBuilder:
+                    (_, index) =>
                 new Dismissible(
-                    key: ObjectKey(_pillsToTake[index].pillName),
-                    child: new PillWidget(pillToTake: _pillsToTake[index], onPillRegimentMetHandler: removePillWhenDailyRegimentHasBeenMet),
+                  key: ObjectKey(state.pillsToTake[index].pillName),
+                  child: new PillWidget(pillToTake: state.pillsToTake[index], onPillRegimentMetHandler: removePillWhenDailyRegimentHasBeenMet),
                   onDismissed: (direction) {
-                      setState(() {
-                        SharedPreferencesService().removePillAtIndexFromDate(
+                    setState(() {
+                      SharedPreferencesService().removePillAtIndexFromDate(
                           index, DateService().getDateAsMonthAndDay(widget.date)
-                        );
-                        _pillsToTake.removeAt(index);
-                      });
+                      );
+                      state.pillsToTake.removeAt(index);
+                    });
                   },
                 )
-        ),
-      )
-    );
+            ),
+          )
+      );
+    }
+    else {
+      return const Text("Something went wrong");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-      return new Container(
-        child:
+    return BlocBuilder<PillBloc, PillState>(
+      builder: (context, state) {
+        return new Container(
+            child:
             new Expanded(
                 child: new SizedBox(
                   height: double.infinity,
@@ -94,21 +107,23 @@ class DayWidgetState extends State<DayWidget> {
                       new Align(
                         alignment: Alignment.topCenter,
                         child:  new Padding(
-                            padding: const EdgeInsets.only(
-                                top:40.0
-                            ),
+                          padding: const EdgeInsets.only(
+                              top:40.0
+                          ),
                           child: new Text(
                               DateService().getDateAsMonthAndDay(widget.date),
                               style: new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)
                           ),
                         ),
                       ),
-                      drawPills(),
+                      drawPills(state),
                     ],
                   ),
                 )
             )
-      );
+        );
+      },
+    );
   }
 
 }
