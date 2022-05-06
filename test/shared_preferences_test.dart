92d412576c6a7cbdc8eb00c337113648d2f5effb
unknown <tomerpacific@gmail.com>
2022-05-06 20:41:05 +0300

    Pill test for adding a pill to shared preferences

--- test/shared_preferences_test.dart
+++ test/shared_preferences_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pill/model/pill_to_take.dart';
 import 'package:pill/service/shared_preferences_service.dart';
@@ -5,14 +7,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 void main() {
-  
-  setUp(() {
+
+  late String date;
+
+  setUp(() async {
+    await SharedPreferencesService().init();
     SharedPreferences.setMockInitialValues({});
+    date = "5/6";
   });
 
   test("SharedPreferences Service get pills for date empty", () {
-      List<PillToTake> pills = SharedPreferencesService().getPillsToTakeForDate("5/6");
+      List<PillToTake> pills = SharedPreferencesService().getPillsToTakeForDate(date);
       expect(pills.length, 0);
+  });
+
+
+  test("SharedPreferences Service add pill to date", () {
+    PillToTake pill = new PillToTake(pillName: "Test Pill", pillRegiment: 2, amountOfDaysToTake: 1);
+    SharedPreferencesService().addPillToDates(date, pill);
+    List<PillToTake> pills = SharedPreferencesService().getPillsToTakeForDate(date);
+
+    expect(pills.length, 1);
+
+    List<PillToTake> found = pills.where((element) => element.pillName == pill.pillName).toList();
+
+    expect(found.length, 1);
   });
 
 
