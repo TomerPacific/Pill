b9bce34c7bb4da9f638fd38ae0b8ea3cec53e2bb
unknown <tomerpacific@gmail.com>
2022-05-12 17:53:25 +0300

    Pill adding tests for filling up the form partially

--- test/adding_pill_form_test.dart
+++ test/adding_pill_form_test.dart
@@ -56,4 +56,61 @@ void main() {
     expect(find.text("Please enter a pill name"), findsOneWidget);
 
   });
+
+  testWidgets("Adding Pill Form - Trying to add a pill with only a pill name", (WidgetTester tester) async {
+
+    await tester.pumpWidget(
+        MultiBlocProvider(
+            providers: [BlocProvider(
+                create: (context) => PillBloc())],
+            child: MaterialApp(
+                home: AddingPillForm(DateTime.now())
+            )
+        )
+    );
+
+    await tester.ensureVisible(find.byType(AddingPillForm));
+
+    await tester.enterText(find.byKey(ObjectKey("pillName")), "Test Pill");
+
+    await tester.tap(find.byIcon(Icons.check));
+
+    await tester.pumpAndSettle(const Duration(milliseconds: 100));
+
+    expect(find.text("Please enter a pill name"), findsNothing);
+
+    expect(find.text("Please enter a number representing the amount of pills to take"), findsOneWidget);
+
+  });
+
+  testWidgets("Adding Pill Form - Trying to add a pill without amount of days", (WidgetTester tester) async {
+
+    await tester.pumpWidget(
+        MultiBlocProvider(
+            providers: [BlocProvider(
+                create: (context) => PillBloc())],
+            child: MaterialApp(
+                home: AddingPillForm(DateTime.now())
+            )
+        )
+    );
+
+    await tester.ensureVisible(find.byType(AddingPillForm));
+
+    await tester.enterText(find.byKey(ObjectKey("pillName")), "Test Pill");
+
+    await tester.enterText(find.byKey(ObjectKey("pillRegiment")), "2");
+
+    await tester.tap(find.byIcon(Icons.check));
+
+    await tester.pumpAndSettle(const Duration(milliseconds: 100));
+
+    expect(find.text("Please enter a pill name"), findsNothing);
+
+    expect(find.text("Please enter a number representing the amount of pills to take"), findsNothing);
+
+    expect(find.text("Please enter a number representing the number of days"), findsOneWidget);
+
+  });
+
 }
