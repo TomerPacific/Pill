f5a4bf194ed3f7fad219c56fd5c27d6ac8e06e9d
unknown <tomerpacific@gmail.com>
2022-05-13 23:42:44 +0300

    Pill adding test to click on pill

--- test/pill_widget_test.dart
+++ test/pill_widget_test.dart
@@ -1,74 +1,112 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pill/bloc/pill_bloc.dart';
+import 'package:pill/bloc/pill_event.dart';
+import 'package:pill/bloc/pill_state.dart';
 import 'package:pill/model/pill_to_take.dart';
+import 'package:pill/service/date_service.dart';
+import 'package:pill/widget/pill_widget.dart';
 
 void main() {
 
-  testWidgets("PillWidget Dismiss Pill", (WidgetTester tester) async {
-    PillToTake pillToTake = new PillToTake();
+
+  Widget drawPills(BuildContext context, PillState state) {
+    if (state is PillLoading) {
+      return const CircularProgressIndicator();
+    }
+    if (state is PillLoaded) {
+      return state.pillsToTake.length == 0 ?
+      new Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: new Text(
+              "You do not have to take any pills today 😀",
+              style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+          )
+      )
+          :
+      Expanded(
+          child: SizedBox(
+            height: 200.0,
+            child:  ListView.builder(
+                itemCount: state.pillsToTake.length,
+                itemBuilder:
+                    (_, index) =>
+                new Dismissible(
+                    key: ObjectKey(state.pillsToTake[index].pillName),
+                    child: new PillWidget(pillToTake: state.pillsToTake[index]),
+                    onDismissed: (direction) {
+                      context.read<PillBloc>().add(DeletePill(pillToTake: state.pillsToTake[index]));
+                      //state.pillsToTake.removeAt(index);
+                    }
+                )
+            ),
+          )
+      );
+    }
+    else {
+      return const Text("Something went wrong");
+    }
+  }
+
+
+  testWidgets("PillWidget Click On Pill", (WidgetTester tester) async {
+    PillToTake pillToTake = new PillToTake(
+        pillRegiment: 1,
+        pillName: "Test Pill",
+        amountOfDaysToTake: 1);
 
     await tester.pumpWidget(
-        MaterialApp(
-            home: Material(
-                child: new Container(
-                  child: new Dismissible(
-                    key: ObjectKey(pillToTake.pillName),
-                    child:new Card(
-                    child: InkWell(
-                        splashColor: Colors.blue.withAlpha(30),
-                        onTap: () {
-
-                        },
-                        child: new Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              new Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    new Text(
-                                      pillToTake.pillName,
-                                      style: new TextStyle(
-                                          fontSize: 20.0,
-                                          fontWeight: FontWeight.bold
-                                      ),
-                                    )
-                                  ]
-                              ),
-                              new Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                      pillToTake.pillImage,
-                                      width: 100,
-                                      height: 100
-                                  )
-                                ],
-                              ),
-                              new Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  new Text(
-                                      "Pills left to take today: ${pillToTake
-                                          .pillRegiment}",
-                                      style: new TextStyle(
-                                          fontSize: 20.0,
-                                          fontWeight: FontWeight.bold
-                                      )
-                                  )
-                                ],
-                              )
-                            ]
-                        )
+        MultiBlocProvider(
+            providers: [BlocProvider(
+                create: (context) => PillBloc()..add(LoadPill()),)],
+            child: MaterialApp(
+                home: BlocBuilder<PillBloc, PillState>(
+                builder: (context, state) {
+                  return new Container(
+                      child:new SizedBox(
+                      height: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                      new Align(
+                      alignment: Alignment.topCenter,
+                      child:  new Padding(
+                        padding: const EdgeInsets.only(
+                            top:40.0
+                        ),
+                        child: new Text(
+                            DateService().getDateAsMonthAndDay(DateTime.now()),
+                            style: new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)
+                        ),
+                      ),
                     ),
-                  ),
+                    drawPills(context, state)
+                    ]
                   )
-                )
+                    )
+                  );
+                }
             )
         )
-    );
+    ));
 
-    await tester.drag(find.byType(Dismissible), const Offset(500.0, 0.0));
-    await tester.pumpAndSettle();
-    expect(find.text('Random Pill'), findsNothing);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    BuildContext context = tester.element(find.byType(Container));
+
+    context.read<PillBloc>().add(AddPill(pillToTake: pillToTake));
+
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    await tester.ensureVisible(find.byType(PillWidget));
+
+    await tester.tap(find.byType(PillWidget));
+
+    await tester.pumpAndSettle(const Duration(milliseconds: 100));
+
+    expect(find.text('Test Pill'), findsNothing);
   });
 }
