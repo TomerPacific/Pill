9a90b135582d1696f6431366374d5cf1cdcb4064
unknown <tomerpacific@gmail.com>
2022-11-13 22:35:14 +0200

    Pill using clear all pills event

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pill/bloc/pill/pill_bloc.dart';
+import 'package:pill/bloc/pill/pill_event.dart';
 
 class SettingsPage extends StatelessWidget {
 
@@ -13,7 +16,7 @@ class SettingsPage extends StatelessWidget {
                           Icons.clear,
                           color: Colors.redAccent),
                       onTap: () {
-
+                        BlocProvider.of<PillBloc>(context).add(ClearAllPills());
                       }
                   ),
                 ]
