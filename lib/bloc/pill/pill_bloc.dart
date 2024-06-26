f312de0c252ae7787b674af99477bea3ab37d126
unknown <tomerpacific@gmail.com>
2024-06-26 17:25:16 +0300

    Pill refactoring pill bloc

--- lib/bloc/pill/pill_bloc.dart
+++ lib/bloc/pill/pill_bloc.dart
@@ -3,81 +3,97 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pill/model/pill_taken.dart';
 import 'package:pill/service/date_service.dart';
 import 'package:pill/service/shared_preferences_service.dart';
-import 'pill_event.dart';
 import 'package:pill/bloc/pill/pill_state.dart';
 import 'package:pill/model/pill_to_take.dart';
 
-class PillBloc extends Bloc<PillEvent, PillState> {
-  PillBloc() : super(PillLoading()) {
-    on<LoadPill>(_onLoadPills);
-    on<AddPill>(_onAddPill);
-    on<UpdatePill>(_onUpdatePill);
-    on<DeletePill>(_onDeletePill);
-    on<ClearAllPills>(_onClearAllPills);
+
+enum PillEvent {loadPills, addPill, removePill, updatePill}
+
+class PillsEvent {
+  final PillEvent eventName;
+  final PillToTake? pillToTake;
+  final List<PillToTake>? pillsToTake;
+  final List<PillTaken>? pillsTaken;
+
+  PillsEvent({
+    required this.eventName,
+    this.pillToTake,
+    this.pillsToTake,
+    this.pillsTaken
+  });
+}
+
+
+class PillBloc extends Bloc<PillsEvent, PillState> {
+  PillBloc(SharedPreferencesService sharedPreferencesService) : super(
+    PillState()
+  ) {
+    on<PillsEvent>((event, emit) async {
+      switch(event.eventName) {
+        case PillEvent.loadPills:
+          _onLoadPills(event, emit);
+          break;
+        case PillEvent.addPill:
+          _onAddPill(event, emit);
+          break;
+        case PillEvent.removePill:
+          _onDeletePill(event, emit, sharedPreferencesService);
+          break;
+        case PillEvent.updatePill:
+          _onUpdatePill(event, emit, sharedPreferencesService);
+          break;
+      }
+    });
   }
 
-  void _onLoadPills(LoadPill event, Emitter<PillState> emitter) {
-    emitter(PillLoaded(
-        pillsToTake: event.pillsToTake,
-        pillsTaken: event.pillsTaken), );
-  }
-
-  void _onAddPill(AddPill event, Emitter<PillState> emitter) {
-    final state = this.state;
-    if (state is PillLoaded) {
-      emitter(PillLoaded(
-          pillsToTake: List.from(state.pillsToTake)..add(event.pillToTake),
-          pillsTaken: state.pillsTaken
-        )
+  void _onLoadPills(PillsEvent event, Emitter<PillState> emitter) {
+    if (event.pillsTaken != null && event.pillsToTake != null) {
+      emitter(new PillState(
+          pillsToTake: event.pillsToTake!,
+          pillsTaken: event.pillsTaken!),
       );
     }
   }
 
-  void _onUpdatePill(UpdatePill event, Emitter<PillState> emitter) {
-    final state = this.state;
-    if (state is PillLoaded) {
+  void _onAddPill(PillsEvent event, Emitter<PillState> emitter) {
+    emitter(new PillState(
+        pillsToTake: List.from(event.pillsToTake!)..add(event.pillToTake!),
+        pillsTaken: state.pillsTaken
+      )
+    );
+  }
+
+  void _onUpdatePill(
+      PillsEvent event,
+      Emitter<PillState> emitter,
+      SharedPreferencesService sharedPreferencesService) async {
+
       List<PillToTake> updatedPills;
-      if (event.pillToTake.pillRegiment == 0) {
-        updatedPills = state.pillsToTake.where((pill) => !pill.equals(event.pillToTake)).toList();
+      if (event.pillToTake!.pillRegiment == 0) {
+        updatedPills = event.pillsToTake!.where((pill) => !pill.equals(event.pillToTake!)).toList();
       } else {
-        updatedPills = state.pillsToTake.map((pill) =>
-        pill.equals(event.pillToTake)
-            ? event.pillToTake
+        updatedPills = event.pillsToTake!.map((pill) =>
+        pill.equals(event.pillToTake!)
+            ? event.pillToTake!
             : pill).toList();
       }
 
     String date = DateService().getCurrentDateAsMonthAndDay();
-    List<PillTaken> pillsTaken = SharedPreferencesService().getPillsTakenForDate(date);
-    emitter(PillLoaded(
+    List<PillTaken> pillsTaken = await sharedPreferencesService.getPillsTakenForDate(date);
+    emitter(PillState(
         pillsToTake: updatedPills,
         pillsTaken: pillsTaken),
     );
-    }
   }
 
-  void _onDeletePill(DeletePill event, Emitter<PillState> emitter) {
-    final state = this.state;
-    if (state is PillLoaded) {
-      List<PillToTake> updatedPills = state.pillsToTake.where((pill) => !pill.equals(event.pillToTake)).toList();
-      String date = DateService().getCurrentDateAsMonthAndDay();
-      List<PillTaken> pillsTaken = SharedPreferencesService().getPillsTakenForDate(date);
-      emitter(PillLoaded(
-          pillsToTake: updatedPills,
-          pillsTaken: pillsTaken),
-      );
-    }
-  }
-
-  void _onClearAllPills(ClearAllPills event, Emitter<PillState> emitter) {
-    final state = this.state;
-    if (state is PillLoaded) {
-      String date = DateService().getCurrentDateAsMonthAndDay();
-      List<PillTaken> pillsTaken = SharedPreferencesService().getPillsTakenForDate(date);
-        emitter(PillLoaded(
-          pillsToTake: const <PillToTake>[],
-          pillsTaken: pillsTaken),
-        );
-    }
+  void _onDeletePill(PillsEvent event, Emitter<PillState> emitter, SharedPreferencesService sharedPreferencesService) async {
+    List<PillToTake> updatedPills = state.pillsToTake!.where((pill) => !pill.equals(event.pillToTake!)).toList();
+    String date = DateService().getCurrentDateAsMonthAndDay();
+    List<PillTaken> pillsTaken = await sharedPreferencesService.getPillsTakenForDate(date);
+    emitter(PillState(
+        pillsToTake: updatedPills,
+        pillsTaken: pillsTaken),
+    );
   }
 
 }
--- lib/bloc/pill/pill_state.dart
+++ lib/bloc/pill/pill_state.dart
@@ -2,19 +2,15 @@
 import 'package:pill/model/pill_taken.dart';
 import 'package:pill/model/pill_to_take.dart';
 
-abstract class PillState {
-  const PillState();
 
-  List<Object> get props => [];
-}
+class PillState {
+  final List<PillToTake>? pillsToTake;
+  final List<PillTaken>? pillsTaken;
+  final PillToTake? pillToTake;
 
-class PillLoading extends PillState {}
-
-class PillLoaded extends PillState {
-  final List<PillToTake> pillsToTake;
-  final List<PillTaken> pillsTaken;
-
-  const PillLoaded({this.pillsToTake = const <PillToTake>[], this.pillsTaken = const <PillTaken>[]});
-
-  List<Object> get props => [pillsToTake, pillsTaken];
+    PillState({
+      this.pillsToTake,
+      this.pillsTaken,
+      this.pillToTake
+    });
 }
