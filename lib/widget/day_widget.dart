03c9d1e7ac2d5dbfdbfde706ebdc1e70389d233b
unknown <tomerpacific@gmail.com>
2024-06-28 19:23:11 +0300

    Pill refactoring logic for pills to take and pills taken

--- lib/widget/day_widget.dart
+++ lib/widget/day_widget.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pill/bloc/pill/pill_bloc.dart';
 import 'package:pill/bloc/pill/pill_state.dart';
-import 'package:pill/model/pill_taken.dart';
-import 'package:pill/model/pill_to_take.dart';
 import 'package:pill/service/date_service.dart';
 import 'package:pill/widget/pill_taken_widget.dart';
 import 'package:pill/widget/pill_to_take_widget.dart';
@@ -16,45 +14,6 @@ class DayWidget extends StatelessWidget {
   final DateTime date;
   final String title;
 
-  Widget _buildPillList(BuildContext context, List<dynamic> pills) {
-    if (pills is List<PillToTake> && pills.length > 0) {
-      return Expanded(
-          child: SizedBox(
-            height: 200.0,
-            child:  ListView.builder(
-                itemCount: pills.length,
-                itemBuilder:
-                    (_, index) =>
-                new Dismissible(
-                    key: ObjectKey(pills[index].pillName),
-                    child: new PillWidget(pillToTake: pills[index]),
-                    onDismissed: (direction) {
-                      context.read<PillBloc>().add(PillsEvent(
-                          eventName: PillEvent.removePill,
-                          date: date.toString(),
-                          pillToTake: pills[index]));
-                      //state.pillsToTake.removeAt(index);
-                    }
-                )
-            ),
-          )
-      );
-    } else if (pills is List<PillTaken> && pills.length > 0) {
-      return Expanded(
-          child: SizedBox(
-            height: 200.0,
-            child:  ListView.builder(
-                itemCount: pills.length,
-                itemBuilder:
-                    (_, index) =>
-                    new PillTakenWidget(pillToTake: pills[index]),
-                )
-            ),
-          );
-    } else {
-      return SizedBox.shrink();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -81,15 +40,16 @@ class DayWidget extends StatelessWidget {
                           ),
                         ),
                       ),
+                      (this.title == "You do not have to take any pills today 😀") ?
                       (state.pillsToTake == null || state.pillsToTake!.isEmpty) ?
-                        new Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: new Text(
-                                title,
-                                style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                            )
-                        )
-                      :
+                      new Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: new Text(
+                              title,
+                              style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                          )
+                      )
+                          :
                       Expanded(
                           child: SizedBox(
                             height: 200.0,
@@ -111,6 +71,27 @@ class DayWidget extends StatelessWidget {
                                 )
                             ),
                           )
+                      )
+                          :
+                      (state.pillsTaken == null || state.pillsTaken!.isEmpty) ?
+                      new Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: new Text(
+                              title,
+                              style: new TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                          )
+                      )
+                          :
+                      Expanded(
+                          child: SizedBox(
+                          height: 200.0,
+                          child:  ListView.builder(
+                            itemCount: state.pillsTaken!.length,
+                            itemBuilder:
+                                (_, index) =>
+                            new PillTakenWidget(pillToTake: state.pillsTaken![index]),
+                          )
+                      ),
                       )
                     ],
                   ),
