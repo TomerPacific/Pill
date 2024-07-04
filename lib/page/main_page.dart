c323ac7dcfcb354fe17df34d1ba7c39f9b8df1c3
unknown <tomerpacific@gmail.com>
2024-07-04 22:47:45 +0300

    Pill formatting file

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -10,9 +10,8 @@ import 'package:pill/widget/day_widget.dart';
 import 'package:pill/widget/adding_pill_form.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage({
-    required this.title,
-    required this.sharedPreferencesService}) : super();
+  MainPage({required this.title, required this.sharedPreferencesService})
+      : super();
 
   final String title;
   final SharedPreferencesService sharedPreferencesService;
@@ -21,8 +20,8 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin {
-
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
   late TabController _controller;
 
   void _handleAddPillButtonPressed() {
@@ -35,23 +34,23 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
   @override
   void initState() {
     super.initState();
-    BlocProvider.of<PillBloc>(context).add(new PillsEvent(eventName: PillEvent.loadPillsToTake,
+    BlocProvider.of<PillBloc>(context).add(new PillsEvent(
+        eventName: PillEvent.loadPillsToTake,
         date: DateService().getCurrentDateAsMonthAndDay()));
     widget.sharedPreferencesService.clearPillsOfPastDays();
     BlocProvider.of<ClearPillsBloc>(context).add(ClearPillsEvent.Init);
     _controller = TabController(length: 3, vsync: this);
     _controller.addListener(() {
-      switch(_controller.index) {
+      switch (_controller.index) {
         case 0:
           context.read<PillBloc>().add(PillsEvent(
               eventName: PillEvent.loadPillsToTake,
-              date: DateService().getCurrentDateAsMonthAndDay())
-          );
+              date: DateService().getCurrentDateAsMonthAndDay()));
           break;
         case 1:
           context.read<PillBloc>().add(PillsEvent(
-      eventName: PillEvent.loadTakenPills,
-      date: DateService().getCurrentDateAsMonthAndDay()));
+              eventName: PillEvent.loadTakenPills,
+              date: DateService().getCurrentDateAsMonthAndDay()));
           break;
       }
     });
@@ -59,53 +58,48 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
-          return Scaffold(
-              appBar: PreferredSize(
-                preferredSize: Size.fromHeight(50),
-                child: AppBar(
-                  bottom: TabBar(
-                    tabs: [
-                    Tab(icon: Icon(CustomIcons.pill)),
-                    Tab(icon: Icon(Icons.watch_later_rounded)),
-                    Tab(icon: Icon(Icons.settings)),
-                  ],
-                    controller: _controller,
-                  ),
-                ),
-              ),
-              body: TabBarView(
-                  controller: _controller,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        new DayWidget(
-                            date: DateTime.now(),
-                            title:  "You do not have to take any pills today 😀"
-                        ),
-                        new Align(
-                          alignment: Alignment.bottomRight,
-                          child: new FloatingActionButton(
-                              onPressed: _handleAddPillButtonPressed,
-                              child: Icon(Icons.add)
-                          ),
-                        )
-
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        new DayWidget(
-                            date: DateTime.now(),
-                            title:  "You have not taken any pills today"),
-                      ],
-                    ),
-                    BlocProvider.value(value: BlocProvider.of<ClearPillsBloc>(context),
-                      child: SettingsPage(sharedPreferencesService: widget.sharedPreferencesService),
-                    )
-                  ]
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(CustomIcons.pill)),
+                Tab(icon: Icon(Icons.watch_later_rounded)),
+                Tab(icon: Icon(Icons.settings)),
+              ],
+              controller: _controller,
+            ),
+          ),
+        ),
+        body: TabBarView(controller: _controller, children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new DayWidget(
+                  date: DateTime.now(),
+                  title: "You do not have to take any pills today 😀"),
+              new Align(
+                alignment: Alignment.bottomRight,
+                child: new FloatingActionButton(
+                    onPressed: _handleAddPillButtonPressed,
+                    child: Icon(Icons.add)),
               )
-          );
-      }
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new DayWidget(
+                  date: DateTime.now(),
+                  title: "You have not taken any pills today"),
+            ],
+          ),
+          BlocProvider.value(
+            value: BlocProvider.of<ClearPillsBloc>(context),
+            child: SettingsPage(
+                sharedPreferencesService: widget.sharedPreferencesService),
+          )
+        ]));
+  }
 }
