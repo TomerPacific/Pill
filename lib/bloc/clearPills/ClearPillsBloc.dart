fe12c306f0698801cc324b6c7ced7d0adc35b67b
unknown <tomerpacific@gmail.com>
2024-07-05 12:45:50 +0300

    Pill refactoring events

--- lib/bloc/clearPills/ClearPillsBloc.dart
+++ lib/bloc/clearPills/ClearPillsBloc.dart
@@ -1,18 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pill/service/shared_preferences_service.dart';
 
-enum ClearPillsEvent { Init, ClearedPills }
+enum ClearPillsEvent { PillsUpdated, ClearedPills }
 
 class ClearPillsBloc extends Bloc<ClearPillsEvent, bool> {
   ClearPillsBloc(SharedPreferencesService sharedPreferencesService) : super(false) {
     on<ClearPillsEvent>((event, emit) async {
-      if (event == ClearPillsEvent.Init) {
-        bool anyPillsLeftToTake = await sharedPreferencesService.areThereAnyPillsToTake();
-        emit(anyPillsLeftToTake);
-      }
-      if (event == ClearPillsEvent.ClearedPills) {
-        sharedPreferencesService.clearAllPills();
-        emit(false);
+      switch (event) {
+        case ClearPillsEvent.PillsUpdated:
+          bool anyPillsLeftToTake = await sharedPreferencesService.areThereAnyPillsToTake();
+          emit(anyPillsLeftToTake);
+          break;
+        case ClearPillsEvent.ClearedPills:
+          await sharedPreferencesService.clearAllPills();
+          emit(false);
+          break;
       }
     });
   }
