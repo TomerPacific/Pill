0c6210aac3049a7626f78f1883cf088091c03e4d
unknown <tomerpacific@gmail.com>
2024-07-06 16:06:53 +0300

    Pill formatting models

--- lib/model/pill_taken.dart
+++ lib/model/pill_taken.dart
@@ -10,43 +10,43 @@ class PillTaken {
   String description;
   DateTime? lastTaken;
 
-  PillTaken({
-    this.pillName = "Random Pill",
-    this.pillWeight = 0,
-    this.description = "Description",
-    this.lastTaken
-  });
+  PillTaken(
+      {this.pillName = "Random Pill",
+      this.pillWeight = 0,
+      this.description = "Description",
+      this.lastTaken});
 
   factory PillTaken.fromJson(Map<String, dynamic> jsonData) {
     return PillTaken(
         pillName: jsonData[PILL_NAME_KEY],
         pillWeight: jsonData[PILL_WEIGHT_KEY],
         description: jsonData[PILL_DESCRIPTION_KEY],
-        lastTaken: jsonData[PILL_LAST_TAKEN_KEY] == null ? null : DateTime.parse(jsonData[PILL_LAST_TAKEN_KEY])
-    );
+        lastTaken: jsonData[PILL_LAST_TAKEN_KEY] == null
+            ? null
+            : DateTime.parse(jsonData[PILL_LAST_TAKEN_KEY]));
   }
 
   static PillTaken extractFromPillToTake(PillToTake pillToTake) {
     return PillTaken(
-      pillName: pillToTake.pillName,
-      pillWeight: pillToTake.pillWeight,
-      description: pillToTake.description,
-      lastTaken: pillToTake.lastTaken
-    );
+        pillName: pillToTake.pillName,
+        pillWeight: pillToTake.pillWeight,
+        description: pillToTake.description,
+        lastTaken: pillToTake.lastTaken);
   }
 
   static Map<String, dynamic> toMap(PillTaken pill) => {
-    PILL_NAME_KEY: pill.pillName,
-    PILL_WEIGHT_KEY: pill.pillWeight,
-    PILL_DESCRIPTION_KEY: pill.description,
-    PILL_LAST_TAKEN_KEY: pill.lastTaken == null ? null : pill.lastTaken!.toIso8601String()
-  };
+        PILL_NAME_KEY: pill.pillName,
+        PILL_WEIGHT_KEY: pill.pillWeight,
+        PILL_DESCRIPTION_KEY: pill.description,
+        PILL_LAST_TAKEN_KEY:
+            pill.lastTaken == null ? null : pill.lastTaken!.toIso8601String()
+      };
 
   static String encode(List<PillTaken> pills) => json.encode(
-    pills
-        .map<Map<String, dynamic>>((pill) => PillTaken.toMap(pill))
-        .toList(),
-  );
+        pills
+            .map<Map<String, dynamic>>((pill) => PillTaken.toMap(pill))
+            .toList(),
+      );
 
   static List<PillTaken> decode(String pills) =>
       (json.decode(pills) as List<dynamic>)
