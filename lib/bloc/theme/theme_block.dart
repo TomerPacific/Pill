9c579fd9cb4edfec15ae6557c69ddf4a111ec85f
unknown <tomerpacific@gmail.com>
2024-07-06 15:44:26 +0300

    Pill formatting theme bloc class

--- lib/bloc/theme/theme_block.dart
+++ lib/bloc/theme/theme_block.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pill/service/shared_preferences_service.dart';
@@ -6,11 +5,14 @@ import 'package:pill/service/shared_preferences_service.dart';
 enum ThemeEvent { toggleDark, toggleLight }
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeMode> {
-  ThemeBloc(SharedPreferencesService sharedPreferencesService, bool isDarkMode) : super(isDarkMode ? ThemeMode.dark : ThemeMode.light) {
+  ThemeBloc(SharedPreferencesService sharedPreferencesService, bool isDarkMode)
+      : super(isDarkMode ? ThemeMode.dark : ThemeMode.light) {
     on<ThemeEvent>((event, emit) {
-      ThemeMode themeMode = event == ThemeEvent.toggleDark ? ThemeMode.dark : ThemeMode.light;
+      ThemeMode themeMode =
+          event == ThemeEvent.toggleDark ? ThemeMode.dark : ThemeMode.light;
       emit(themeMode);
-      sharedPreferencesService.saveThemeStatus(themeMode == ThemeMode.dark ? true : false);
+      sharedPreferencesService
+          .saveThemeStatus(themeMode == ThemeMode.dark ? true : false);
     });
   }
 }
