25f2b90dd8e57ece978711715a46cdfc0a3fed66
unknown <tomerpacific@gmail.com>
2024-07-06 15:43:18 +0300

    Pill formatting class

--- lib/bloc/clearPills/ClearPillsBloc.dart
+++ lib/bloc/clearPills/ClearPillsBloc.dart
@@ -4,11 +4,13 @@ import 'package:pill/service/shared_preferences_service.dart';
 enum ClearPillsEvent { PillsUpdated, ClearedPills }
 
 class ClearPillsBloc extends Bloc<ClearPillsEvent, bool> {
-  ClearPillsBloc(SharedPreferencesService sharedPreferencesService) : super(false) {
+  ClearPillsBloc(SharedPreferencesService sharedPreferencesService)
+      : super(false) {
     on<ClearPillsEvent>((event, emit) async {
       switch (event) {
         case ClearPillsEvent.PillsUpdated:
-          bool anyPillsLeftToTake = await sharedPreferencesService.areThereAnyPillsToTake();
+          bool anyPillsLeftToTake =
+              await sharedPreferencesService.areThereAnyPillsToTake();
           emit(anyPillsLeftToTake);
           break;
         case ClearPillsEvent.ClearedPills:
