cd6f026ddb22904e04b22fa9c7bc3ebd90321ccd
unknown <tomerpacific@gmail.com>
2024-07-07 14:40:02 +0300

    Pill refactoring date service to not be a singleton

--- lib/service/date_service.dart
+++ lib/service/date_service.dart
@@ -1,12 +1,5 @@
 
 class DateService {
-  static final DateService instance = DateService._internal();
-
-  factory DateService() {
-    return instance;
-  }
-
-  DateService._internal();
 
   String getDateAsMonthAndDay(DateTime date) {
     return date.month.toString() + "/" + date.day.toString();
