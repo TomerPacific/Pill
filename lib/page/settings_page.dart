037f31578484f7ebe5a400c78328f82eff280e29
unknown <tomerpacific@gmail.com>
2024-07-10 20:30:59 +0300

    Pill formatting formatting settings screen

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -5,68 +5,57 @@ import 'package:pill/bloc/theme/theme_block.dart';
 import 'package:pill/service/shared_preferences_service.dart';
 
 class SettingsPage extends StatelessWidget {
-
-  SettingsPage({
-    required this.sharedPreferencesService
-  });
+  SettingsPage({required this.sharedPreferencesService});
 
   final SharedPreferencesService sharedPreferencesService;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SwitchListTile(
-              title: const Text("Dark Mode"),
-              secondary: new Icon(
-                  Icons.dark_mode,
-                  color: context.read<ThemeBloc>().state == ThemeMode.dark ?
-                  Color.fromARGB(200, 243, 231, 106) :
-                  Color(0xFF642ef3)
-              ),
-              value: context.read<ThemeBloc>().state == ThemeMode.dark ? true : false,
-              onChanged: (bool isDarkModeEnabled) {
-                ThemeEvent event =
-                context.read<ThemeBloc>().state == ThemeMode.dark
-                    ? ThemeEvent.toggleLight
-                    : ThemeEvent.toggleDark;
-                BlocProvider.of<ThemeBloc>(context).add(event);
-              }),
-          ListTile(
-              title: const Text("Clear All Pills"),
-              leading: const Icon(
-                  Icons.clear,
-                  color: Colors.redAccent),
-              enabled: context.read<ClearPillsBloc>().state,
-              onTap: () {
-                AlertDialog alertDialog = AlertDialog(
-                  title: const Text("Clear All Saved Pills"),
-                  content: const Text("Are you sure you want to remove all your saved pills?"),
-                  actions: [
-                    TextButton(
-                        onPressed: () {
-                          BlocProvider.of<ClearPillsBloc>(context).add(ClearPillsEvent.ClearedPills);
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Yes")
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text("No")
-                    ),
-                  ],
-                );
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return alertDialog;
-                    });
-              }
-          ),
-        ]
-    );
+    return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+      SwitchListTile(
+          title: const Text("Dark Mode"),
+          secondary: new Icon(Icons.dark_mode,
+              color: context.read<ThemeBloc>().state == ThemeMode.dark
+                  ? Color.fromARGB(200, 243, 231, 106)
+                  : Color(0xFF642ef3)),
+          value:
+              context.read<ThemeBloc>().state == ThemeMode.dark ? true : false,
+          onChanged: (bool isDarkModeEnabled) {
+            ThemeEvent event = context.read<ThemeBloc>().state == ThemeMode.dark
+                ? ThemeEvent.toggleLight
+                : ThemeEvent.toggleDark;
+            BlocProvider.of<ThemeBloc>(context).add(event);
+          }),
+      ListTile(
+          title: const Text("Clear All Pills"),
+          leading: const Icon(Icons.clear, color: Colors.redAccent),
+          enabled: context.read<ClearPillsBloc>().state,
+          onTap: () {
+            AlertDialog alertDialog = AlertDialog(
+              title: const Text("Clear All Saved Pills"),
+              content: const Text(
+                  "Are you sure you want to remove all your saved pills?"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      BlocProvider.of<ClearPillsBloc>(context)
+                          .add(ClearPillsEvent.ClearedPills);
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Yes")),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("No")),
+              ],
+            );
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return alertDialog;
+                });
+          }),
+    ]);
   }
 }
