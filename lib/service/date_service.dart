32a2c72aecbbd61c348df407c03e697cf7cfd9c3
unknown <tomerpacific@gmail.com>
2024-07-10 20:35:30 +0300

    Pill formatting class and adding constant

--- lib/service/date_service.dart
+++ lib/service/date_service.dart
@@ -1,19 +1,20 @@
-
 class DateService {
+  final int TEN = 10;
 
   String getDateAsMonthAndDay(DateTime date) {
     return date.month.toString() + "/" + date.day.toString();
   }
 
   String getHourFromDate(DateTime dateTime) {
-    String hour = dateTime.hour < 10 ? "0${dateTime.hour}" : "${dateTime.hour}";
+    String hour =
+        dateTime.hour < TEN ? "0${dateTime.hour}" : "${dateTime.hour}";
     String minutes = _convertDigitToStringWithPadding(dateTime.minute);
     String seconds = _convertDigitToStringWithPadding(dateTime.second);
     return "$hour:$minutes:$seconds";
   }
 
   String _convertDigitToStringWithPadding(int digit) {
-    return digit < 10 ? "0$digit" : "$digit";
+    return digit < TEN ? "0$digit" : "$digit";
   }
 
   String getCurrentDateAsMonthAndDay() {
