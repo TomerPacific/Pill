070879ad3a76416cf5e3f5cb3cad36197f9bb939
unknown <tomerpacific@gmail.com>
2025-05-01 09:53:12 +0300

    Pill removing new reserved word

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:pill/service/shared_preferences_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  DateService dateService = new DateService();
+  DateService dateService = DateService();
   SharedPreferencesService sharedPreferencesService =
       await SharedPreferencesService.create(dateService);
   bool isDarkMode = await sharedPreferencesService.getThemeStatus();
