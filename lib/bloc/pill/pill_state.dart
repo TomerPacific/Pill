180fa0516d43fec095a66be556abe91e50886ecf
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-30 18:42:14 +0300

    Pill extending equatable in pill state

--- lib/bloc/pill/pill_state.dart
+++ lib/bloc/pill/pill_state.dart
@@ -1,10 +1,14 @@
+import 'package:equatable/equatable.dart';
 import 'package:pill/model/pill_taken.dart';
 import 'package:pill/model/pill_to_take.dart';
 
-class PillState {
+class PillState extends Equatable {
   final List<PillToTake>? pillsToTake;
   final List<PillTaken>? pillsTaken;
   final PillToTake? pillToTake;
 
   const PillState({this.pillsToTake, this.pillsTaken, this.pillToTake});
+
+  @override
+  List<Object?> get props => [pillsToTake, pillsTaken, pillToTake];
 }
